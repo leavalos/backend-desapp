709c2b42d5170d6f6a388e687ededfe6f85459ab
Lucas Emiliano Avalos <l.e.avalos1@gmail.com>
2020-09-01 15:43:47 -0300

    Added init Springboot config

---
+++ src/main/kotlin/ar/edu/unq/desapp/grupom/backenddesappapi/BackendDesappApiApplication.kt
@@ -0,0 +1,11 @@
+package ar.edu.unq.desapp.grupom.backenddesappapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendDesappApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendDesappApiApplication>(*args)
+}
